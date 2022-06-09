d8be24ad776dc28d2f25961bcc356df08cbbef7d
thanh quach <thanhb.quach@gmail.com>
2022-06-08 23:17:39 -0400

    basic setup

---
+++ myreactapp/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <h1>thanhQ</h1>
+            <div className="links">
+                <a href="/">About</a>
+                <a href="/projects"> Projects</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
