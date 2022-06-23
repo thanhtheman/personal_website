9867fa4db61f2db08581a399cf92995eb0809d48
thanh quach <thanhb.quach@gmail.com>
2022-06-23 12:01:09 -0400

    mapping

---
+++ myreactapp/src/ProjectDetails.js
@@ -0,0 +1,18 @@
+import rpt from './images/RPT.png';
+
+const projectDetails = [
+{image: rpt,
+ title: "Fractional Real Estate Investment", 
+ type: "Type: Web3",
+ techStack: "Tech: Solidity (smart contracts), Hardhat, MetaMask, EtherJS, React, Material UI, JavaScript",
+ description:"This project uses ERC20 token to tokenize a piece of real property and put it on the blockchain. It's a full-fledged Web 3 app.", 
+ id: 1},
+{title: "NFT Marketplace", 
+type: "Type: Web3", 
+techStack: "Tech: So",
+description:"This project is a MVP of a full-fledged NFT marketplace in which users can create and sell their NFT", 
+id: 2},
+    {title: "Private Memories", type: "Type: Web2", description:"This project is about...", id: 3},
+    {title: "Smart To-Do List", type: "Type: Web2", description:"This project is about...", id: 4}];
+
+export default projectDetails;
--- myreactapp/src/Projects.js
+++ myreactapp/src/Projects.js
@@ -1,18 +1,18 @@
+import projectDetails from "./ProjectDetails";
 
 const Projects = () => {
-
-    const projects = [{title: "Fractional Real Estate Investment", type: "Web3", description:"This project is about", id: 1},
-    {title: "NFT Marketplace", type: "Web3", description:"This project is about...", id: 2},
-    {title: "Private Memories", type: "Web2", description:"This project is about...", id: 3}];
-
     return (
         <div>
             <h2>Projects</h2>
-            {projects.map((project)=>(
+            <br></br>
+            {projectDetails.map((project) => (
                 <div className = "project" key = {project.id}>
+                    <br></br>
+                    <img src={project.image} alt="project logo" />
                     <h3>{project.title}</h3>
                     <p>{project.type}</p>
                     <p>{project.description}</p>
+                    <p>{project.techStack}</p>
                 </div>
             ))}
         </div>
