ffc02897c8cfeb9b812ade250e560b49d96876cb
thanh quach <thanhb.quach@gmail.com>
2022-06-27 06:43:53 -0400

    navbar desktop

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Navbar.scss';
+import { images } from '../../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
+            <img src={images.logo} alt="logo" />
+        </div>
+        <ul className='app__navbar-links'>
+          {['home', 'about', 'projects', 'skills', 'contact'].map((item)=>(
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+    </nav>
   );
 }
 
